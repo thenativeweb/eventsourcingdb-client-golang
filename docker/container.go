0e3e5b41552f8f234258d835f1323464cf1e6716
Noah Hummel <noah.hummel@thenativeweb.io>
2022-10-21 16:43:46 +0200

    chore: restart database for each test (#10)

--- docker/container.go
+++ docker/container.go
@@ -1,5 +1,47 @@
 package docker
 
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
 type Container struct {
 	ID string
+}
+
+func (container Container) Kill() error {
+	cliCommand := exec.Command("docker", "kill", container.ID)
+	err := cliCommand.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (container Container) Restart() error {
+	cliCommand := exec.Command("docker", "restart", container.ID)
+	err := cliCommand.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (container Container) GetExposedPort(internalPort int) (int, error) {
+	cliCommand := exec.Command("docker", "inspect", "--format='{{(index (index .NetworkSettings.Ports \""+strconv.Itoa(internalPort)+"/tcp\") 0).HostPort}}'", container.ID)
+	stdout, err := cliCommand.Output()
+	if err != nil {
+		return 0, err
+	}
+
+	exposedPortAsString := strings.Trim(string(stdout), " '\n\r")
+	exposedPort, err := strconv.Atoi(exposedPortAsString)
+	if err != nil {
+		return 0, err
+	}
+
+	return exposedPort, nil
 }
--- ping_test.go
+++ ping_test.go
@@ -8,21 +8,21 @@ import (
 
 func TestPing(t *testing.T) {
 	t.Run("returns an error if an invalid url is given.", func(t *testing.T) {
-		client := database.WithInvalidURL.Client
+		client := database.WithInvalidURL.GetClient()
 		err := client.Ping()
 
 		assert.Error(t, err)
 	})
 
 	t.Run("supports authorization.", func(t *testing.T) {
-		client := database.WithAuthorization.Client
+		client := database.WithAuthorization.GetClient()
 		err := client.Ping()
 
 		assert.NoError(t, err)
 	})
 
 	t.Run("does not return an error if EventSourcingDB is reachable.", func(t *testing.T) {
-		client := database.WithoutAuthorization.Client
+		client := database.WithoutAuthorization.GetClient()
 		err := client.Ping()
 
 		assert.NoError(t, err)
