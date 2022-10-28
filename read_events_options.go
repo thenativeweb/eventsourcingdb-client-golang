b7f667b6555fa2f9a3c5bcaf3eef0ea448bcf1ce
Noah Hummel <noah.hummel@thenativeweb.io>
2022-10-28 14:26:27 +0200

    chore: extract read_events_options

---
+++ read_events_options.go
@@ -0,0 +1,57 @@
+package eventsourcingdb
+
+type ReadEventsOptionsOrder string
+
+const (
+	OldestFirst ReadEventsOptionsOrder = "oldest-first"
+	NewestFirst ReadEventsOptionsOrder = "newest-first"
+)
+
+type ReadEventsOptions struct {
+	OptionWithSubStreams *bool                   `json:"withSubStreams,omitempty"`
+	OptionOrder          *ReadEventsOptionsOrder `json:"order,omitempty"`
+	OptionEventNames     *[]string               `json:"eventNames,omitempty"`
+	OptionLowerBoundID   *int64                  `json:"lowerBoundId,omitempty"`
+	OptionUpperBoundID   *int64                  `json:"upperBoundId,omitempty"`
+	OptionFromEventName  *string                 `json:"fromEventName,omitempty"`
+}
+
+func NewReadEventsOptions() ReadEventsOptions {
+	return ReadEventsOptions{}
+}
+
+func (options ReadEventsOptions) WithSubStreams(withSubStreams bool) ReadEventsOptions {
+	options.OptionWithSubStreams = &withSubStreams
+
+	return options
+}
+
+func (options ReadEventsOptions) Order(order ReadEventsOptionsOrder) ReadEventsOptions {
+	options.OptionOrder = &order
+
+	return options
+}
+
+func (options ReadEventsOptions) EventNames(eventNames []string) ReadEventsOptions {
+	options.OptionEventNames = &eventNames
+
+	return options
+}
+
+func (options ReadEventsOptions) LowerBoundID(lowerBoundID int64) ReadEventsOptions {
+	options.OptionLowerBoundID = &lowerBoundID
+
+	return options
+}
+
+func (options ReadEventsOptions) UpperBoundID(upperBoundID int64) ReadEventsOptions {
+	options.OptionUpperBoundID = &upperBoundID
+
+	return options
+}
+
+func (options ReadEventsOptions) FromEventName(eventName string) ReadEventsOptions {
+	options.OptionFromEventName = &eventName
+
+	return options
+}
