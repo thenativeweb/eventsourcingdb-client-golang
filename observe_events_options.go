4564f21034b6fac4bbc6e03df471adc8acf6c1f8
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2022-11-11 11:50:18 +0100

    feat: Remove option `eventNames` from read- and observeevents. (#22)
    * feat: Remove option `eventNames` from read- and observeevents.
    
    * chore: Update eventsourcingdb to 0.5.0.

--- observe_events_options.go
+++ observe_events_options.go
@@ -1,22 +1,15 @@
 package eventsourcingdb
 
 type ObserveEventsOptions struct {
-	OptionRecursive     bool      `json:"recursive"`
-	OptionEventNames    *[]string `json:"eventNames,omitempty"`
-	OptionLowerBoundID  *int      `json:"lowerBoundId,omitempty"`
-	OptionFromEventName *string   `json:"fromEventName,omitempty"`
+	OptionRecursive     bool    `json:"recursive"`
+	OptionLowerBoundID  *int    `json:"lowerBoundId,omitempty"`
+	OptionFromEventName *string `json:"fromEventName,omitempty"`
 }
 
 func NewObserveEventsOptions(recursive bool) ObserveEventsOptions {
 	return ObserveEventsOptions{
 		OptionRecursive: recursive,
 	}
-}
-
-func (options ObserveEventsOptions) EventNames(eventNames []string) ObserveEventsOptions {
-	options.OptionEventNames = &eventNames
-
-	return options
 }
 
 func (options ObserveEventsOptions) LowerBoundID(lowerBoundID int) ObserveEventsOptions {
