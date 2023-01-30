a513d52e4d99240614ba61b2dc2e943b6a366da2
Noah Hummel <noah.hummel@thenativeweb.io>
2023-01-30 16:45:29 +0100

    feat: implement event candidate data validation (#56)
    * feat: implement event candidate data validation
    
    TODO: detect circular references
    
    * feat: add cycle detection
    
    * chore: add tests for candidate context validation
    
    * chore: add test for validating event candidate data
    
    * chore: add another test
    
    * chore: add more tests
    
    * chore: clean up
    
    * fix: only get type of valid values
    
    * chore: test cycle detection
    
    * chore: add tests for WriteEvents

--- pkg/eventsourcingdb/write_events_test.go
+++ pkg/eventsourcingdb/write_events_test.go
@@ -53,7 +53,7 @@ func TestWriteEvents(t *testing.T) {
 		)
 
 		assert.True(t, errors.IsInvalidParameterError(err))
-		assert.ErrorContains(t, err, "parameter 'eventCandidates' is invalid: malformed event subject 'foobar': subject must be an absolute, slash-separated path")
+		assert.ErrorContains(t, err, "parameter 'eventCandidates' is invalid: event candidate failed to validate: malformed event subject 'foobar': subject must be an absolute, slash-separated path")
 	})
 
 	t.Run("returns an error if a candidate type is malformed", func(t *testing.T) {
@@ -66,7 +66,7 @@ func TestWriteEvents(t *testing.T) {
 		)
 
 		assert.True(t, errors.IsInvalidParameterError(err))
-		assert.ErrorContains(t, err, "parameter 'eventCandidates' is invalid: malformed event type 'barbaz': type must be reverse domain name")
+		assert.ErrorContains(t, err, "parameter 'eventCandidates' is invalid: event candidate failed to validate: malformed event type 'barbaz': type must be a reverse domain name")
 	})
 
 	t.Run("returns an error if a candidate source is malformed", func(t *testing.T) {
@@ -79,7 +79,22 @@ func TestWriteEvents(t *testing.T) {
 		)
 
 		assert.True(t, errors.IsInvalidParameterError(err))
-		assert.ErrorContains(t, err, "parameter 'eventCandidates' is invalid: malformed event source '://wurstsoße': source must be a valid URI")
+		assert.ErrorContains(t, err, "parameter 'eventCandidates' is invalid: event candidate failed to validate: malformed event source '://wurstsoße': source must be a valid URI")
+	})
+
+	t.Run("returns an error if a candidate's data contains unexported fields.", func(t *testing.T) {
+		client := database.WithoutAuthorization.GetClient()
+
+		_, err := client.WriteEvents(
+			[]event.Candidate{
+				event.NewCandidate("tag:foobar.invalid,2023:service", "/foobar", "com.foobar.barbaz", struct {
+					private string
+				}{}),
+			},
+		)
+
+		assert.True(t, errors.IsInvalidParameterError(err))
+		assert.ErrorContains(t, err, "parameter 'eventCandidates' is invalid: event candidate failed to validate: event data is unsupported: unexported field 'Data.private' is not supported, data must only contain exported fields, or json.Marshaler must be implement on 'struct { private string }'")
 	})
 
 	t.Run("supports authorization.", func(t *testing.T) {
