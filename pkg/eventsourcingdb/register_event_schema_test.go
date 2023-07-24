71fd6c250242d5146a5c396d15a46b4ead39214f
Noah Hummel <hello@strangedev.org>
2023-07-24 15:02:46 +0200

    fix: correctly propagate error from database

--- pkg/eventsourcingdb/register_event_schema_test.go
+++ pkg/eventsourcingdb/register_event_schema_test.go
@@ -24,7 +24,7 @@ func TestClient_RegisterEventSchema(t *testing.T) {
 		assert.NoError(t, err)
 
 		err = client.RegisterEventSchema("com.gornisht.ekht", `{"type": "object", "additionalProperties": false}`)
-		assert.ErrorContains(t, err, "Conflict")
+		assert.ErrorContains(t, err, "client error: Conflict: schema conflicts with existing event (ID=0)")
 	})
 
 	t.Run("Rejects the request if the schema already exists.", func(t *testing.T) {
@@ -34,13 +34,13 @@ func TestClient_RegisterEventSchema(t *testing.T) {
 		assert.NoError(t, err)
 
 		err = client.RegisterEventSchema("com.ekht.ekht", `{"type": "object"}`)
-		assert.ErrorContains(t, err, "Conflict")
+		assert.ErrorContains(t, err, "client error: Conflict: schema already exists")
 	})
 
 	t.Run("Rejects the request if the given schema is invalid.", func(t *testing.T) {
 		client := database.WithAuthorization.GetClient()
 
 		err := client.RegisterEventSchema("com.ekht.ekht", `{"type": `)
-		assert.ErrorContains(t, err, "Bad Request")
+		assert.ErrorContains(t, err, "Bad Request: schema conflicts with existing event (ID=0)")
 	})
 }
