6db18a81eeb256c00e73d4d6d69f54201ee52dd7
Golo Roden <golo.roden@thenativeweb.io>
2023-09-26 09:58:46 +0200

    Update invalid_parameter_error.go

--- pkg/errors/invalid_parameter_error.go
+++ pkg/errors/invalid_parameter_error.go
@@ -10,7 +10,7 @@ import (
 // Invalid parameter errors can generally be handled by the user.
 var ErrInvalidParameter = errors.New("invalid parameter")
 
-// NewInvalidParameterError return a new invalid parameter error that indicates that the given parameter is invalid.
+// NewInvalidParameterError returns a new invalid parameter error that indicates that the given parameter is invalid.
 func NewInvalidParameterError(parameterName, reason string) error {
 	return errors.Join(ErrClientError, ErrInvalidParameter, fmt.Errorf("parameter '%s' is invalid\n%s", parameterName, reason))
 }
