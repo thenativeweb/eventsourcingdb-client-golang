69681de2393bc7a8248a3a53945c1628dda31f46
Kuro <alexander.kampf@thenativeweb.io>
2024-10-17 13:44:29 +0200

    Update eventsourcingdb/read_subjects_test.go
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>

--- eventsourcingdb/read_subjects_test.go
+++ eventsourcingdb/read_subjects_test.go
@@ -215,7 +215,7 @@ func TestReadSubjects(t *testing.T) {
 			assert.Error(t, err)
 			assert.True(t, errors.Is(err, eventsourcingdb.ErrServerError))
 			assert.ErrorContains(t, err, "unexpected response status: 202 Accepted")
-			count += 1
+			count++
 		}
 
 		assert.Equal(t, 1, count)
