ff339ff082763cdf65d9c412ab607093b6784487
Golo Roden <golo.roden@thenativeweb.io>
2025-10-12 12:06:30 +0200

    Simplify error handling.

--- internal/validate_server_header.go
+++ internal/validate_server_header.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 	"strings"
 )
@@ -10,12 +9,8 @@ import (
 func ValidateServerHeader(response *http.Response) error {
 	serverHeader := response.Header.Get("Server")
 
-	if serverHeader == "" {
-		return errors.New("server must be EventSourcingDB, but Server header is missing")
-	}
-
-	if !strings.HasPrefix(serverHeader, "EventSourcingDB/") {
-		return fmt.Errorf("server must be EventSourcingDB, got Server header: '%s'", serverHeader)
+	if serverHeader == "" || !strings.HasPrefix(serverHeader, "EventSourcingDB/") {
+		return errors.New("server must be EventSourcingDB")
 	}
 
 	return nil
